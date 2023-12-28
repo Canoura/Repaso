355c19bb33f7462a1ded754d284853a3fb148c9c
Canoura <sthefanyacostac@gmail.com>
2023-12-28 14:12:10 +0000

    Bucle  que coge los 3 numeros introducidos y los va comparando entre ellos para ordenarlos de  mayor a menor

--- ejercicio2.js
+++ ejercicio2.js
@@ -3,5 +3,25 @@ const resultado=document.getElementById("resultado");
 
 
 function myFunction() {
-    let número1 = prompt("Introduce El primer número");
+    let numero1 = prompt("Introduce El primer número");
+    let numero2 = prompt("Introduce El segundo número");
+    let numero3 = prompt("Introduce El tercer número");
+
+    numerosintrododucidos.textContent=`los números introducidos son ${numero1}, ${numero2}, ${numero3}`;
+
+if(numero1>=numero2 && numero1>=numero3 && numero2>=numero3){
+    resultado.textContent = `El orden es: ${numero1}, ${numero2}, ${numero3}`;
+}else if(numero1>=numero2 && numero1>=numero3 && numero2<=numero3){
+    resultado.textContent = `El orden es: ${numero1}, ${numero3}, ${numero2}`;
+
+}else if(numero2>=numero1 && numero2>=numero3 && numero1>=numero3){
+    resultado.textContent = `El orden es: ${numero2}, ${numero1}, ${numero3}`;
+}else if(numero2>=numero1 && numero2>=numero3 && numero1<=numero3){
+    resultado.textContent = `El orden es: ${numero2}, ${numero3}, ${numero1}`;
+
+}else if(numero3>=numero1 && numero3>=numero2 && numero1>=numero2){
+    resultado.textContent = `El orden es: ${numero3}, ${numero1}, ${numero2}`;
+}else if(numero3>=numero1 && numero3>=numero2 && numero1<=numero2){
+    resultado.textContent = `El orden es: ${numero3}, ${numero2}, ${numero1}`;
+}
 }
