d385e9bfecb315b56610555cfc61da8e4647fa5c
Canoura <sthefanyacostac@gmail.com>
2023-12-28 14:22:11 +0000

    modificando textos de salida

--- ejercicio2.js
+++ ejercicio2.js
@@ -7,21 +7,21 @@ function myFunction() {
     let numero2 = prompt("Introduce El segundo número");
     let numero3 = prompt("Introduce El tercer número");
 
-    numerosintrododucidos.textContent=`los números introducidos son ${numero1}, ${numero2}, ${numero3}`;
+    numerosintrododucidos.textContent=`LOS NUMERO INTRODUCIDOS SON LOS SIGUIENTES: ${numero1}, ${numero2}, ${numero3}`;
 
 if(numero1>=numero2 && numero1>=numero3 && numero2>=numero3){
-    resultado.textContent = `El orden es: ${numero1}, ${numero2}, ${numero3}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero1}, ${numero2}, ${numero3}`;
 }else if(numero1>=numero2 && numero1>=numero3 && numero2<=numero3){
-    resultado.textContent = `El orden es: ${numero1}, ${numero3}, ${numero2}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero1}, ${numero3}, ${numero2}`;
 
 }else if(numero2>=numero1 && numero2>=numero3 && numero1>=numero3){
-    resultado.textContent = `El orden es: ${numero2}, ${numero1}, ${numero3}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero2}, ${numero1}, ${numero3}`;
 }else if(numero2>=numero1 && numero2>=numero3 && numero1<=numero3){
-    resultado.textContent = `El orden es: ${numero2}, ${numero3}, ${numero1}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero2}, ${numero3}, ${numero1}`;
 
 }else if(numero3>=numero1 && numero3>=numero2 && numero1>=numero2){
-    resultado.textContent = `El orden es: ${numero3}, ${numero1}, ${numero2}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero3}, ${numero1}, ${numero2}`;
 }else if(numero3>=numero1 && numero3>=numero2 && numero1<=numero2){
-    resultado.textContent = `El orden es: ${numero3}, ${numero2}, ${numero1}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero3}, ${numero2}, ${numero1}`;
 }
 }
