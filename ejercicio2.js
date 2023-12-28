42d29c62f20fd5f87cf63c570976aba18ae71486
Canoura <sthefanyacostac@gmail.com>
2023-12-28 13:48:15 +0000

    Añadiendo los ficheros de mi proyecto ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+const numerosintrododucidos=document.getElementById("numerosintroducidos");
+const resultado=document.getElementById("resultado");
+
+
+function myFunction() {
+    let número1 = prompt("Introduce El primer número");
+}
