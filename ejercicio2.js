9c251704075298d32cac215030738bcef9a0fd15
Canoura <sthefanyacostac@gmail.com>
2024-01-04 10:59:54 +0000

    Modificacmos el codigo eliminando los promp , y cogiendo los datos directamente de los introducidos en la ventana de html

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,27 +1,25 @@
-const numerosintrododucidos=document.getElementById("numerosintroducidos");
+//const numerosintrododucidos=document.getElementById("numerosintroducidos");
 const resultado=document.getElementById("resultado");
 
 
 function myFunction() {
-    let numero1 = prompt("Introduce El primer número");
-    let numero2 = prompt("Introduce El segundo número");
-    let numero3 = prompt("Introduce El tercer número");
-
-    numerosintrododucidos.textContent=`LOS NUMERO INTRODUCIDOS SON LOS SIGUIENTES: ${numero1}, ${numero2}, ${numero3}`;
-
+    const numero1 = parseInt(document.getElementById('numero1').value);
+    const numero2 = parseInt(document.getElementById('numero2').value);
+    const numero3 = parseInt(document.getElementById('numero3').value);
+    
 if(numero1>=numero2 && numero1>=numero3 && numero2>=numero3){
-    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero1}, ${numero2}, ${numero3}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR DE LOS NÚMEROS INTRODUCIDOS ES: ${numero1}, ${numero2}, ${numero3}`;
 }else if(numero1>=numero2 && numero1>=numero3 && numero2<=numero3){
-    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero1}, ${numero3}, ${numero2}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR DE LOS NÚMEROS INTRODUCIDOS ES: ${numero1}, ${numero3}, ${numero2}`;
 
 }else if(numero2>=numero1 && numero2>=numero3 && numero1>=numero3){
-    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero2}, ${numero1}, ${numero3}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR DE LOS NÚMEROS INTRODUCIDOS ES: ${numero2}, ${numero1}, ${numero3}`;
 }else if(numero2>=numero1 && numero2>=numero3 && numero1<=numero3){
-    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero2}, ${numero3}, ${numero1}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR DE LOS NÚMEROS INTRODUCIDOS ES: ${numero2}, ${numero3}, ${numero1}`;
 
 }else if(numero3>=numero1 && numero3>=numero2 && numero1>=numero2){
-    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero3}, ${numero1}, ${numero2}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR  DE LOS NÚMEROS INTRODUCIDOS ES: ${numero3}, ${numero1}, ${numero2}`;
 }else if(numero3>=numero1 && numero3>=numero2 && numero1<=numero2){
-    resultado.textContent = `EL ORDEN DE MAYOR A MENOR ES: ${numero3}, ${numero2}, ${numero1}`;
+    resultado.textContent = `EL ORDEN DE MAYOR A MENOR DE LOS NÚMEROS INTRODUCIDOS ES: ${numero3}, ${numero2}, ${numero1}`;
 }
 }
